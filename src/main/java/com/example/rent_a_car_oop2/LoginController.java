64176c693be1296b839a38dd44430023516b734c
OGSmurfen <114438460+OGSmurfen@users.noreply.github.com>
2023-02-05 19:34:57 +0200

    Rent-a-car
    OOP java rent a car project

---
+++ src/main/java/com/example/rent_a_car_oop2/LoginController.java
@@ -0,0 +1,76 @@
+package com.example.rent_a_car_oop2;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+
+import java.io.File;
+import java.net.URL;
+
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+
+    @FXML
+    private Button cancelButton;
+    @FXML
+    private Label loginMessageLabel;
+    @FXML
+    private ImageView brandingImageView;
+    @FXML
+    private ImageView lockImageView;
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private PasswordField enterPasswordField;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        File brandingFile = new File("Images/login_rentacar_2.png");
+        Image brandingImage = new Image(brandingFile.toURI().toString());
+        brandingImageView.setImage(brandingImage);
+
+        File lockFile = new File("Images/logn_lok-removebg-preview.png");
+        Image lockImage = new Image(lockFile.toURI().toString());
+        lockImageView.setImage(lockImage);
+
+        loginMessageLabel.setVisible(false);
+    }
+
+    public void loginButtonOnAction(ActionEvent event){
+        //loginMessageLabel.setText("");
+        if(usernameTextField.getText().isBlank() == false && enterPasswordField.getText().isBlank() == false){
+            validateLogin();
+            loginMessageLabel.setText("Let's go!");
+            loginMessageLabel.setVisible(true);
+        }else{
+            loginMessageLabel.setVisible(true);
+        }
+
+    }
+
+    public void cancelButtonOnAction(ActionEvent event){
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
+    }
+    public void validateLogin(){
+
+    }
+
+    /* original:
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }*/
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,23 @@
+module com.example.rent_a_car_oop2 {
+
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires java.sql;
+    //requires mysql.connector.java;  ne raboti?!?!
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.rent_a_car_oop2 to javafx.fxml;
+    exports com.example.rent_a_car_oop2;
+    /*original:
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.rent_a_car_oop2 to javafx.fxml;
+    exports com.example.rent_a_car_oop2;*/
+}
